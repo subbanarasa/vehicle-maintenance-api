d3c7e2a5152da5d28df331e54ccaf93204edfef6
subbanarasa.puttana <subbanarasa.puttana@cgi.com>
2020-12-06 18:26:04 +0530

    Initial spring template

---
+++ src/main/java/com/interview/assignment/vehiclemaintenence/VehicleMaintenenceApplication.java
@@ -0,0 +1,13 @@
+package com.interview.assignment.vehiclemaintenence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleMaintenenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleMaintenenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/assignment/vehiclemaintenence/VehicleMaintenenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.assignment.vehiclemaintenence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleMaintenenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
